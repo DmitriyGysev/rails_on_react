8c4a05707fda04a3619be4c5460bc5d3e3e1c6aa
Dmitry Gusev <dmitriy.gusev@duniceedge.com>
2017-07-19 00:11:11 +0300

    Set webpuck and all envs

---
+++ client/app/components/Static.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Static extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name: this.props.name };
+  }
+
+  updateName = (name) => {
+    this.setState({ name });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>
+          Hello, {this.state.name}!
+        </h3>
+        <hr />
+        <form >
+          <label htmlFor="name">
+            Say hello to:
+          </label>
+          <input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={(e) => this.updateName(e.target.value)}
+          />
+        </form>
+      </div>
+    );
+  }
+}
---
+++ client/app/startup/registration.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Static from '../components/Static';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  Static
+});
